d8d809daee3c04b37c348f19d279ab64cef673b4
Nadav Ebert <nadavebert69@gmail.com>
2019-12-26 22:24:01 +0200

    finished hold feature

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 var scores,
+	holds,
+	holdValue,
+	nodeText,
 	currentScore,
 	currentPlayer,
 	dice,
@@ -8,19 +11,50 @@ var scores,
 	btnNewGame,
 	playerContainers,
 	finalScore,
-	domScores;
-initGame();
+	gameIsPlaying,
+	domScores,
+	domCurrentScores,
+	domFinalScore,
+	domDotContainers,
+	domDots,
+	node,
+	DEFAULT_FINAL_VALUE,
+	HOLD_COUNT;
+DEFAULT_FINAL_VALUE = 50;
+HOLD_COUNT = 3;
+
 domDice = document.querySelector('.dice-img');
 btnRoll = document.querySelector('.btn-roll');
 btnHold = document.querySelector('.btn-hold');
 btnNewGame = document.querySelector('.btn-new');
 playerContainers = document.querySelectorAll('.container-player');
 domScores = document.querySelectorAll('.player-score');
+domCurrentScores = document.querySelectorAll('.score');
+domFinalScoreInput = document.getElementById('final-score');
+//omDotContainers = document.querySelectorAll('.dot-container');
+domDots = document.querySelectorAll('.dot');
+initGame();
 btnRoll.addEventListener('click', function() {
-	finalScore = document.getElementById('final-score').value;
-	console.log(finalScore);
-	roll();
+	finalScore = domFinalScoreInput.value;
+	if (!finalScore) {
+		finalScore = DEFAULT_FINAL_VALUE;
+		domFinalScoreInput.value = DEFAULT_FINAL_VALUE;
+	} else if (!gameIsPlaying) {
+		finalScore = domFinalScoreInput.value;
+		domFinalScoreInput.value = finalScore;
+	}
+	domFinalScoreInput.disabled = true;
+	gameIsPlaying = true;
+	holdValue = Math.floor(finalScore / 10);
 
+	if (!btnHold.textContent.includes('+')) {
+		nodeText = ' (+' + holdValue + ')';
+		node = document.createTextNode(nodeText);
+		//	btnHold.removeChild(node);
+		btnHold.appendChild(node);
+	}
+	console.log('final score: ' + finalScore);
+	roll();
 	if (dice > 1) {
 		currentScore += dice;
 		if (currentScore + scores[currentPlayer] >= finalScore) {
@@ -35,10 +69,14 @@ btnRoll.addEventListener('click', function() {
 });
 
 btnHold.addEventListener('click', function() {
-	scores[currentPlayer] += currentScore;
-	manipulateDom('hold');
-	switchCurrentPlayer();
-	currentScore = 0;
+	if (holds[currentPlayer] > 0 && currentScore > 0) {
+		scores[currentPlayer] += currentScore + holdValue;
+		holds[currentPlayer]--;
+		console.log('hold count: ' + holds[currentPlayer]);
+		manipulateDom('hold');
+		switchCurrentPlayer();
+		currentScore = 0;
+	}
 });
 btnNewGame.addEventListener('click', function() {
 	manipulateDom('new game');
@@ -50,33 +88,48 @@ function manipulateDom(action) {
 	var activeDomScore = document.querySelector('.active .player-score');
 	var activeDomCurrentScore = document.querySelector('.active .score');
 	var activeH1 = document.querySelector('.active .player-name h1');
-	var activeDot = document.querySelector('.active .dot');
+	var activeDomDots = document.querySelectorAll('.active .dot');
 	if (action === 'addCurrentScore') {
 		activeDomCurrentScore.textContent = currentScore;
 	} else if (action === 'hold') {
 		activeDomScore.textContent = scores[currentPlayer];
 		activeDomCurrentScore.textContent = '0';
+		var holdsToRemove = HOLD_COUNT - holds[currentPlayer];
+		for (var i = 0; i < holdsToRemove; i++) {
+			if (activeDomDots[HOLD_COUNT - i - 1].classList.contains('hold'))
+				activeDomDots[HOLD_COUNT - i - 1].classList.remove('hold');
+		}
 		hideDice();
 		animate(activeDomScore);
 		switchCurrentPlayerDom();
 	} else if (action === 'winner') {
 		activeH1.textContent = 'winner!';
-		activeDot.style.display = 'none';
+		activeDomDots.forEach((dot) => {
+			dot.style.display = 'none';
+		});
+
 		activeDomScore.textContent = currentScore + scores[currentPlayer];
 		activeDomCurrentScore.textContent = '0';
 		animate(activeDomScore);
 		disableButtons();
 	} else if (action === 'new game') {
 		activeH1.textContent = 'player ' + (currentPlayer + 1);
+		if (btnHold.textContent.includes('+')) btnHold.removeChild(node);
 		if (!playerContainers[0].classList.contains('active')) {
 			switchCurrentPlayerDom();
 		}
 		domScores.forEach((score) => {
 			score.textContent = '0';
 		});
-		activeDot.style.display = 'block';
+		domCurrentScores.forEach((score) => {
+			score.textContent = '0';
+		});
+		activeDomDots.forEach((dot) => {
+			dot.style.display = 'block';
+		});
 		activateButtons();
 		hideDice();
+		initGame();
 	} else {
 		activeDomScore.textContent = '0';
 		activeDomCurrentScore.textContent = '0';
@@ -110,7 +163,7 @@ function hideDice() {
 }
 function roll() {
 	dice = Math.floor(Math.random() * 6) + 1;
-	console.log(dice);
+	console.log('roll: ' + dice);
 }
 function disableButtons() {
 	btnRoll.style.display = 'none';
@@ -125,5 +178,11 @@ function initGame() {
 	currentPlayer = 0;
 	currentScore = 0;
 	scores = [ 0, 0 ];
+	holds = [ HOLD_COUNT, HOLD_COUNT ];
 	dice = 0;
+	gameIsPlaying = false;
+	domFinalScoreInput.disabled = false;
+	domDots.forEach((dot) => {
+		for (var i = 0; i < HOLD_COUNT * 2; i++) dot.classList.add('hold');
+	});
 }
